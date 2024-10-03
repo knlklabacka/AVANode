364f5a9e6994466b323eb16c2b81d0b602e76987
knlklabacka <ATSIntranet+\knlklabacka@users.noreply.github.com>
2024-10-03 10:28:23 -0500

    added main.js to test https server

---
+++ main.js
@@ -0,0 +1,16 @@
+// import https module
+const https = require('https');
+
+//import fs module to read files
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('./openssl/ssl.key'),
+    cert: fs.readFileSync('./openssl/ssl.crt')
+};
+
+// create https server port 3000
+https.createServer(options, (req, ers) => {
+    resizeBy.writeHead(200);
+    resizeBy.send('Hello world from https server \n');
+}).listen(3000);
